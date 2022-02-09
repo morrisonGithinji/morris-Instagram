b2551d791e13ad17e99258186a4473acd8d3075a
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2022-02-09 07:48:59 +0300

    and sender on email

--- instaclone/email.py
+++ instaclone/email.py
@@ -3,8 +3,8 @@ from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
-    subject = 'Welcome to Instacity,for allyour Photo uploads and also get to view your friends photos'
-    sender = 'ebenezergakuo3@gmail.com'
+    subject = 'Welcome to Morrisinsta,for allyour Photo uploads and also get to view your friends photos'
+    sender = 'morrison.githinji@student.moringaschool.com'
 
     #passing in the context vairables
     text_content = render_to_string('email/instaemail.txt',{"name": name})
